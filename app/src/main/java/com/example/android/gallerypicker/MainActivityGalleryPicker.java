faebf79601b95216e73cfe6f31055a1767136f19
christinav3 <christinav204@gmail.com>
2018-07-25 23:53:01 -0700

    turn image into bitmap and get touch coord

--- app/src/main/java/com/example/android/gallerypicker/MainActivityGalleryPicker.java
+++ app/src/main/java/com/example/android/gallerypicker/MainActivityGalleryPicker.java
@@ -2,14 +2,19 @@ package com.example.android.gallerypicker;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivityGalleryPicker extends AppCompatActivity {
 
@@ -41,23 +46,65 @@ public class MainActivityGalleryPicker extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == this.requestCode) {
             Uri selectedImage = data.getData();
-            ImageView image = (ImageView) findViewById(R.id.image);
-            //new <code>
-            image.setImageBitmap(bitmap);
+            //turn into bitmap
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            image = (ImageView) findViewById(R.id.image);
+            image.setImageURI(selectedImage);
 
             //applying grayscale
-            ColorMatrix matrix = new ColorMatrix();
+           /* ColorMatrix matrix = new ColorMatrix();
             matrix.setSaturation(0);
             ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
-            image.setColorFilter(filter);
-            image.setImageURI(selectedImage);
+            image.setColorFilter(filter);*/
+
+            image.setOnTouchListener(handleTouch);
         }
     }
 
 
-    //public Bitmap toGrayscale(Bitmap bmp) {
+    //get x, y coordinates of touch event
+    private View.OnTouchListener handleTouch = new View.OnTouchListener() {
 
-    //}
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            Matrix m = new Matrix();
+            image.getImageMatrix().invert(m);
+            float[] pts = { event.getX(), event.getY()};
+            m.mapPoints(pts);
+            //Log.d("TAG", "onTouch x: " + Math.floor(pts[0]) + ", y: " + Math.floor(pts[1]));
+
+
+            int x = (int) pts[0];
+            int y = (int) pts[1];
+
+            if(x < 0 || x >= bitmap.getWidth() || y < 0 || y >= bitmap.getHeight()) {
+                return false;
+            }
+
+            int color = bitmap.getPixel(x,y);
+            int red = Color.red(color);
+            int green = Color.green(color);
+            int blue = Color.blue(color);
+
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    Log.i("TAG", " get x and y: ( " + x + " , " + y + ") ");
+                    Log.i("TAG", "red: "+ red +" blue: "+blue +", green: "+green +" " );
+                    break;
+            }
+
+            return true;
+        }
+
+    };
 
 
 }
